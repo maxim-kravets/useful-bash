62889d2b65e3a5e115aca7d5cd040c5e796f5b8a
maxim-kravets <max@deovr.com>
2019-02-28 00:54:44 +0200

    add-new-ssh-key-to-github.sh updated, verification if git installed added

--- add-new-ssh-key-to-github.sh
+++ add-new-ssh-key-to-github.sh
@@ -2,17 +2,29 @@
 
 set -e
 
-GIT_USERNAME=$1
+$(git --version &>/dev/null)
+GIT_INSTALLED=$?
 
-if [ -z $GIT_USERNAME ]; then
+if [ $GIT_INSTALLED -ne 0 ]; then
+    printf "\nGit is not installed. You need to install it first.\n\n"
 
-	GIT_USERNAME=$(git config user.name)
+    exit 1
+fi
 
-	if [ -z $GIT_USERNAME ]; then
-	    printf "\nYour git configuration doesn't have username.\nPass the username of github account as first argument:\n./add-new-ssh-key-to-github.sh <your_github_username>\n\n"
-	    exit 1
-	fi
+git_username=$1
+
+if [ -z $git_username ]; then
+
+	git_username=$(git config user.name)
 	
+fi
+
+if [ -z $git_username ]; then
+    printf "\nYour git configuration doesn't have username.\n"
+    printf "Pass the username of github account as first argument:\n"
+    printf "$0 <your-github-username>\n\n"
+
+    exit 1
 fi
 
 ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -q -N ""
@@ -20,6 +32,6 @@ ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -q -N ""
 TITLE="Key created by bash script at $(date '+%d-%m-%Y %H:%M:%S')"
 KEY=$(cat ~/.ssh/id_rsa.pub)
 TEMPLATE='{"title":"%s","key":"%s"}'
-JSON_STRING=$(printf "$TEMPLATE" "$TITLE" "$KEY")
+BODY=$(printf "$TEMPLATE" "$TITLE" "$KEY")
 
-curl -d "$JSON_STRING" -H "Content-Type: application/json" -X POST -u $GIT_USERNAME https://api.github.com/user/keys
+curl -d "$BODY" -H "Content-Type: application/json" -X POST -u $git_username https://api.github.com/user/keys
