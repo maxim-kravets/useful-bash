afd4c238b54c87d70183cd4ededcd6057f17f1fd
maxim-kravets <max@deovr.com>
2019-02-28 13:02:16 +0200

    add-new-ssh-key-to-github.sh updated

--- add-new-ssh-key-to-github.sh
+++ add-new-ssh-key-to-github.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-$(git --version &>/dev/null)
+git --version > /dev/null 2>&1
 GIT_INSTALLED=$?
 
-if [ $GIT_INSTALLED -ne 0 ]; then
+if [[ "$GIT_INSTALLED" -ne 0 ]]; then
     printf "\nGit is not installed. You need to install it first.\n\n"
 
     exit 1
@@ -13,13 +13,13 @@ fi
 
 git_username=$1
 
-if [ -z $git_username ]; then
+if [[ -z "$git_username" ]]; then
 
 	git_username=$(git config user.name)
 	
 fi
 
-if [ -z $git_username ]; then
+if [[ -z "$git_username" ]]; then
     printf "\nYour git configuration doesn't have username.\n"
     printf "Pass the username of github account as first argument:\n"
     printf "$0 <your-github-username>\n\n"
@@ -34,4 +34,4 @@ KEY=$(cat ~/.ssh/id_rsa.pub)
 TEMPLATE='{"title":"%s","key":"%s"}'
 BODY=$(printf "$TEMPLATE" "$TITLE" "$KEY")
 
-curl -d "$BODY" -H "Content-Type: application/json" -X POST -u $git_username https://api.github.com/user/keys
+curl -d "$BODY" -H "Content-Type: application/json" -X POST -u "$git_username" https://api.github.com/user/keys
